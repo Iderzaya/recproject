4405536a52704675ae0f4398c00ed4ac3502936d
Iderzaya <78403828+Iderzaya@users.noreply.github.com>
2023-05-03 10:27:38 +0800

    Update teal_python.py

--- teal_python.py
+++ teal_python.py
@@ -1,6 +1,8 @@
 from pyteal import *
 
 
+# 207610354
+
 def approval():
     reserve_address = App.globalGet(Bytes("Reserve"))
 
@@ -9,13 +11,14 @@ def approval():
         return Seq(
             [
                 Assert(
-                    Txn.application_args.length() == Int(0),
-                    Txn.type_enum() == TxnType.ApplicationCall,
-                    Txn.fee() == Global.min_txn_fee(),
-                    Txn.close_remainder_to() == Global.zero_address(),
-                    Txn.rekey_to() == Global.zero_address(),
-                    Global.group_size() == Int(1)
-                ),
+                    And(
+                        Txn.application_args.length() == Int(0),
+                        Txn.type_enum() == TxnType.ApplicationCall,
+                        Txn.fee() == Global.min_txn_fee(),
+                        Txn.close_remainder_to() == Global.zero_address(),
+                        Txn.rekey_to() == Global.zero_address(),
+                        Global.group_size() == Int(1)
+                    )),
                 App.globalPut(Bytes("Creator"), Txn.sender()),
                 Return(Int(1))
             ]
@@ -23,7 +26,7 @@ def approval():
 
     def deleteorupdate():
         return Seq(
-            Assert(
+            Assert(And(
                 App.globalGet(Bytes("Creator")) == Txn.sender(),
                 Txn.type_enum() == TxnType.ApplicationCall,
                 Txn.fee() == Global.min_txn_fee(),
@@ -31,7 +34,7 @@ def approval():
                 Txn.close_remainder_to() == Global.zero_address(),
                 Txn.asset_close_to() == Global.zero_address(),
                 Txn.rekey_to() == Global.zero_address(),
-                Global.group_size() == Int(1)
+                Global.group_size() == Int(1))
             ),
             Approve()
         )
@@ -40,24 +43,25 @@ def approval():
     def asset_creation():
         return Seq(
             Assert(
-                App.globalGet(Bytes("Creator")) == Txn.sender(),
-                Txn.on_completion() == OnComplete.NoOp,
-                Txn.type_enum() == TxnType.ApplicationCall,
-                Txn.fee() == Global.min_txn_fee(),
-                Txn.close_remainder_to() == Global.zero_address(),
-                Txn.asset_close_to() == Global.zero_address(),
-                Txn.rekey_to() == Global.zero_address(),
-                Txn.application_id() == Global.current_application_id(),
-                Global.group_size() == Int(1)
+                And(
+                    App.globalGet(Bytes("Creator")) == Txn.sender(),
+                    Txn.on_completion() == OnComplete.NoOp,
+                    Txn.type_enum() == TxnType.ApplicationCall,
+                    Txn.fee() == Global.min_txn_fee(),
+                    Txn.close_remainder_to() == Global.zero_address(),
+                    Txn.asset_close_to() == Global.zero_address(),
+                    Txn.rekey_to() == Global.zero_address(),
+                    Txn.application_id() == Global.current_application_id(),
+                    Global.group_size() == Int(1))
             ),
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields({
                 TxnField.type_enum: TxnType.AssetConfig,
                 TxnField.fee: Global.min_txn_fee(),
                 TxnField.config_asset_total: Int(100000000000000000),
-                TxnField.config_asset_decimals: Int(0),
+                TxnField.config_asset_decimals: Int(4),
                 TxnField.config_asset_unit_name: Bytes("rec"),
-                TxnField.config_asset_name: Bytes("RecToken2504"),
+                TxnField.config_asset_name: Bytes("RecAsset"),
                 TxnField.config_asset_default_frozen: Int(1),
                 TxnField.config_asset_manager: App.globalGet(Bytes("Creator")),
                 TxnField.config_asset_reserve: Txn.accounts[1],
@@ -73,7 +77,7 @@ def approval():
     # Make sure to opt in reserve acc to the asset, beforehand
     def reserve():
         return Seq(
-            Assert(
+            Assert(And(
                 App.globalGet(Bytes("Creator")) == Txn.sender(),
                 Global.group_size() == Int(1),
                 Txn.on_completion() == OnComplete.NoOp,
@@ -84,7 +88,7 @@ def approval():
                 Txn.asset_close_to() == Global.zero_address(),
                 Txn.rekey_to() == Global.zero_address(),
                 Txn.assets[0] == App.globalGet(Bytes("asset")),
-                Txn.accounts[1] == App.globalGet(Bytes("Reserve"))
+                Txn.accounts[1] == App.globalGet(Bytes("Reserve")))
             ),
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields({
@@ -104,18 +108,19 @@ def approval():
     def transfer():
         return Seq(
             Assert(
-                App.globalGet(Bytes("Creator")) == Txn.sender(),
-                Global.group_size() == Int(1),
-                Txn.type_enum() == TxnType.ApplicationCall,
-                Txn.on_completion() == OnComplete.NoOp,
-                Txn.fee() == Global.min_txn_fee(),
-                Txn.close_remainder_to() == Global.zero_address(),
-                Txn.asset_close_to() == Global.zero_address(),
-                Txn.rekey_to() == Global.zero_address(),
-                Txn.application_id() == Global.current_application_id(),
-                Txn.assets[0] == App.globalGet(Bytes("asset")),
-                Txn.accounts[3] == App.globalGet(Bytes("Reserve")),
-                Btoi(Txn.application_args[2]) == Int(0)
+                And(
+                    App.globalGet(Bytes("Creator")) == Txn.sender(),
+                    Global.group_size() == Int(1),
+                    Txn.type_enum() == TxnType.ApplicationCall,
+                    Txn.on_completion() == OnComplete.NoOp,
+                    Txn.fee() == Global.min_txn_fee(),
+                    Txn.close_remainder_to() == Global.zero_address(),
+                    Txn.asset_close_to() == Global.zero_address(),
+                    Txn.rekey_to() == Global.zero_address(),
+                    Txn.application_id() == Global.current_application_id(),
+                    Txn.assets[0] == App.globalGet(Bytes("asset")),
+                    Txn.accounts[3] == App.globalGet(Bytes("Reserve")),
+                    Btoi(Txn.application_args[1]) > Int(0))
             ),
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields({
@@ -130,41 +135,40 @@ def approval():
             Approve()
         )
 
-    # Comparing 2 inputs and checking if they are within the tolerance of 2%
-    tolerance = Seq(
-        If(Btoi(Txn.application_args[1]) / Btoi(Txn.application_args[2]) * Int(100) <= Int(102),
-           App.globalPut(Bytes("checker"), Int(1)), App.globalPut(Bytes("checker"), Int(0))),
-        If(Btoi(Txn.application_args[1]) / Btoi(Txn.application_args[2]) * Int(100) <= Int(102),
-           App.globalPut(Bytes("checker"), Int(1)), App.globalPut(Bytes("checker"), Int(0))))
+    def tol():
+        return (Btoi(Txn.application_args[1]) - Btoi(Txn.application_args[2])) * Int(100) <= Btoi(
+            Txn.application_args[3]) * Btoi(Txn.application_args[1])
 
     # Minting given amount of asset, which means taking from reserve account and putting into circulation on blockchain
     def mint():
         return Seq(
             Assert(
-                App.globalGet(Bytes("Creator")) == Txn.sender(),
-                Global.group_size() == Int(1),
-                Txn.on_completion() == OnComplete.NoOp,
-                Txn.type_enum() == TxnType.ApplicationCall,
-                Txn.application_id() == Global.current_application_id(),
-                Txn.fee() == Global.min_txn_fee(),
-                Txn.close_remainder_to() == Global.zero_address(),
-                Txn.asset_close_to() == Global.zero_address(),
-                Txn.rekey_to() == Global.zero_address(),
-                Txn.assets[0] == App.globalGet(Bytes("asset")),
-                Txn.accounts[2] == App.globalGet(Bytes("Reserve")),
-                Btoi(Txn.application_args[1]) > Int(0),
-                Btoi(Txn.application_args[2]) > Int(0),
-                Btoi(Txn.application_args[3]) > Int(0),
+                And(
+                    App.globalGet(Bytes("Creator")) == Txn.sender(),
+                    Global.group_size() == Int(1),
+                    Txn.on_completion() == OnComplete.NoOp,
+                    Txn.type_enum() == TxnType.ApplicationCall,
+                    Txn.application_id() == Global.current_application_id(),
+                    Txn.fee() == Global.min_txn_fee(),
+                    Txn.close_remainder_to() == Global.zero_address(),
+                    Txn.asset_close_to() == Global.zero_address(),
+                    Txn.rekey_to() == Global.zero_address(),
+                    Txn.assets[0] == App.globalGet(Bytes("asset")),
+                    Txn.accounts[2] == App.globalGet(Bytes("Reserve")),
+                    Btoi(Txn.application_args[1]) > Int(0),
+                    Btoi(Txn.application_args[2]) > Int(0),
+                    Btoi(Txn.application_args[3]) > Int(0),
+                    Btoi(Txn.application_args[4]) > Int(0),
+                )
             ),
-            tolerance,
-            Assert(App.globalGet(Bytes("checker")) == Int(1)),
+            Assert(tol() == Int(1)),
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields({
                 TxnField.type_enum: TxnType.AssetTransfer,
                 TxnField.fee: Global.min_txn_fee(),
                 TxnField.asset_sender: reserve_address,
                 TxnField.asset_receiver: Txn.accounts[1],
-                TxnField.asset_amount: Btoi(Txn.application_args[3]),
+                TxnField.asset_amount: Btoi(Txn.application_args[4]),
                 TxnField.xfer_asset: App.globalGet(Bytes("asset")),
             }),
             InnerTxnBuilder.Submit(),
@@ -175,18 +179,20 @@ def approval():
     def retire():
         return Seq(
             Assert(
-                App.globalGet(Bytes("Creator")) == Txn.sender(),
-                Global.group_size() == Int(1),
-                Txn.application_id() == Global.current_application_id(),
-                Txn.type_enum() == TxnType.ApplicationCall,
-                Txn.on_completion() == OnComplete.NoOp,
-                Txn.fee() == Global.min_txn_fee(),
-                Txn.close_remainder_to() == Global.zero_address(),
-                Txn.asset_close_to() == Global.zero_address(),
-                Txn.rekey_to() == Global.zero_address(),
-                Txn.assets[0] == App.globalGet(Bytes("asset")),
-                Txn.accounts[2] == App.globalGet(Bytes("Reserve")),
-                Btoi(Txn.application_args[2]) > Int(0)
+                And(
+                    App.globalGet(Bytes("Creator")) == Txn.sender(),
+                    Global.group_size() == Int(1),
+                    Txn.application_id() == Global.current_application_id(),
+                    Txn.type_enum() == TxnType.ApplicationCall,
+                    Txn.on_completion() == OnComplete.NoOp,
+                    Txn.fee() == Global.min_txn_fee(),
+                    Txn.close_remainder_to() == Global.zero_address(),
+                    Txn.asset_close_to() == Global.zero_address(),
+                    Txn.rekey_to() == Global.zero_address(),
+                    Txn.assets[0] == App.globalGet(Bytes("asset")),
+                    Txn.accounts[2] == App.globalGet(Bytes("Reserve")),
+                    Btoi(Txn.application_args[1]) > Int(0)
+                )
             ),
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields({
